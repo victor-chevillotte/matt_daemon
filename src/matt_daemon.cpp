21b0ea6333d3a3e9d516385172d514137a74c141
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-03-12 14:22:32 +0100

    style: removed comments

--- src/matt_daemon.cpp
+++ src/matt_daemon.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include<unistd.h>
 
 int main() {
     const int PORT = 4242;
@@ -9,8 +9,19 @@ int main() {
 
     // TODO: Write the lock file
 
-    // TODO: Fork the process and quit the parent process
+    // Fork the process and quit the parent process, leaving child process as daemon
+    pid_t c_pid = fork(); 
+    if (c_pid == -1) { 
+        perror("fork"); 
+        exit(EXIT_FAILURE); 
+    } 
+    else if (c_pid > 0) { 
+        std::cout << "Parent process is exiting" << std::endl;
+        exit(EXIT_SUCCESS);
+    } 
 
+    std::cout << "Daemon process is running" << std::endl; 
+  
     // TODO: Catch signals
 
     // TODO: Create the socket
@@ -20,6 +31,9 @@ int main() {
     std::cout << "Listening on port " << PORT << std::endl;
 
     // TODO: Log loop
+    while (1) {
+        sleep(1);
+    }
 
     return 0;
 }
