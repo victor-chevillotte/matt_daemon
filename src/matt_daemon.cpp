78ddbfd6614456e1ec0f10d4743afea4d1b67360
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2024-03-14 10:47:39 +0100

    fix: server creation exception catch

--- src/matt_daemon.cpp
+++ src/matt_daemon.cpp
@@ -23,6 +23,7 @@ void ft_exit(int status) {
 }
 
 void signalHandler( int signum ) {
+    Server::_running = false;
     Tintin_reporter* reporter = Tintin_reporter::GetInstance();
     reporter->log_to_file("INFO", "Signal handler.\n");
     ft_exit(signum);
@@ -52,25 +53,32 @@ int main() {
     }
 
     reporter->log_to_file("INFO", "Creating server\n");
-    Server server = Server(std::to_string(PORT));
-    reporter->log_to_file("INFO", "Server created\n");
+    try {
+        Server server = Server(std::to_string(PORT));
+        reporter->log_to_file("INFO", "Server created\n");
 
-    reporter->log_to_file("INFO", "Entering daemon mode\n");
-    // Fork the process and quit the parent process, leaving child process as daemon
-    pid_t c_pid = fork(); 
-    if (c_pid == -1) { 
-        perror("fork"); 
-        exit(EXIT_FAILURE); 
-    } 
-    else if (c_pid > 0) { 
-        exit(EXIT_SUCCESS);
+        // Fork the process and quit the parent process, leaving child process as daemon
+        reporter->log_to_file("INFO", "Entering daemon mode\n");
+        pid_t c_pid = fork(); 
+        if (c_pid == -1) { 
+            perror("fork"); 
+            exit(EXIT_FAILURE); 
+        } 
+        else if (c_pid > 0) { 
+            exit(EXIT_SUCCESS);
+        }
+        reporter->log_to_file("INFO", "Started. PID: " + std::to_string(getpid()) + "\n");
+    
+        // Catch all signals
+        set_signals();
+
+        server.start();
+
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        reporter->log_to_file("ERROR", "Server runtime error.\n");
+        return 1;
     }
-    reporter->log_to_file("INFO", "Started. PID: " + std::to_string(getpid()) + "\n");
-  
-    // Catch all signals
-    set_signals();
-
-    server.start();
     
     return 0;
 }
